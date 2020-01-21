bcabf83dae3d0a0b511f922120680c8926887791
Joshua Park <jspark103@gmail.com>
2020-01-21 15:33:57 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import "./App.css";
 
 // Your Tasks:
-// 1. import Button component to this (App.js) file and render it
-// 2. Create a <button> element that says "Click Me"
-// 3. Add an onClick handler to the button that alerts "You clicked me!"
-// 4. Import <TaskList> component to this (App.js) file and render it.
-// 5. Center align the the h1 title and TaskList component
+// 1. Create a <button> element that says "Click Me".
+// 2. Add an onClick handler to the button that alerts "You clicked me!".
+// 3. Import <TaskList> component to this (App.js) file and render it.
+// 4. Center align the the h1 title and TaskList component.
+// 5. Style h1 title font-color to green.
+// 6. Increase the width of the button to 300px.
 
 //Utilize Chrome Dev Tools to debug and view your HTML/CSS
 
 function App() {
   return (
     <div>
-      <h1>Welcome to your playground!!</h1>
+      <h1>Welcome to your playground!! Good Luck!</h1>
     </div>
   );
 }
