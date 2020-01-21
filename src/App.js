a37a301037c34d6c044cd9885278a48843da4b18
Joshua Park <jspark103@gmail.com>
2020-01-21 15:15:32 -0800

    more setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
-import Button from "./components/Button";
+
+// Your Tasks:
+// 1. import Button component to this (App.js) file and render it
+// 2. Create a <button> element that says "Click Me"
+// 3. Add an onClick handler to the button that alerts "You clicked me!"
+// 4. Import <TaskList> component to this (App.js) file and render it.
+// 5. Center align the the h1 title and TaskList component
+
+//Utilize Chrome Dev Tools to debug and view your HTML/CSS
+
 function App() {
   return (
     <div>
       <h1>Welcome to your playground!!</h1>
-      <Button />
     </div>
   );
 }
---
+++ src/components/TaskList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TaskList() {
+  return (
+    <div>
+      <li>Swoop</li>
+      <li>Is</li>
+      <li>The</li>
+      <li>Best!</li>
+    </div>
+  );
+}
+
+export default TaskList;
