ccb60fde873a6aeb6a8cde667ae55c9cccc7a4be
Joshua Park <jspark103@gmail.com>
2020-01-24 11:20:07 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import "./App.css";
 
 // Your Tasks:
 // 1. Create your feature branch. Name it "your-name-feature-branch"
-// 2. Spin up a localhost server (see how in README.md)
+// 2. Spin up a localhost server.
 // 3. Create a <button> element that says "Click Me".
 // 4. Add an onClick handler to the button that alerts "You clicked me!".
 // 5. Import <List> component to this (App.js) file and render it.
 // 6. Center align our content (h1, button and List).
-// 7. Style h1 title color to green.
-// 8. Increase the width of the button to 300px.
-// 8.5 **Bonus Challenge** Pass a prop down (string) from the Parent component (app.js) to child (List) and render it as the last task in the List
+// 7. Create a 300x300 <div> inside existing div, set a bg color and center it.
+// 8.5 **Bonus Challenge** Add another <li> to our <List> component by passing a prop down to from Parent (app.js) to child (List.js)
 // 9. Add and commit your changes.
 
 //Utilize Chrome Dev Tools to debug and view your HTML/CSS
